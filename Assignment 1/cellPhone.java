a0fd3709c462edbddf45a80179be2a86c3232825
Haani Khan <107712538+khanhaani22@users.noreply.github.com>
2026-01-30 04:36:57 +0000

    aaa

---
+++ Assignment 1/cellPhone.java
@@ -0,0 +1,35 @@
+public class cellPhone
+{
+    private String manufact;
+    private int model;
+    private double retailPrice;
+
+    public cellPhone()
+    {
+        manufact = "";
+        model = 0;
+        retailPrice = 0.0;
+    }
+
+    public cellPhone(String manufact, int model, double retailPrice)
+    {
+        this.manufact = manufact;
+        this.model = model;
+        this.retailPrice = retailPrice;
+    }
+
+    public void setManufact(String manufact)
+    { this.manufact = manufact;}
+
+    public void setModel(int model)
+    { this.model = model;}
+
+    public void setRetailPrice(double retailPrice)
+    { this.retailPrice = retailPrice;}
+
+    public String toString()
+    {
+        String message = "Manufacturer: " + manufact + "\nModel: " + model + "\nRetail Price: " + retailPrice;
+        return message;
+    }
+}
---
+++ Assignment 1/cellPhoneDemo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class cellPhoneDemo {
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String manuf;
+        int mod;
+        double price;
+
+        System.out.print("Cell Phone Class \nEnter Manufacturer: ");
+        manuf = scan.nextLine();
+
+        System.out.print("\nEnter Model: ");
+        mod = scan.nextInt();
+
+        System.out.print("\nEnter Retail Price: ");
+        price = scan.nextDouble();
+
+        cellPhone phone = new cellPhone(manuf, mod, price);
+        System.out.println(phone.toString());
+    }
+}
