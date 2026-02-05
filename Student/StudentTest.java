d946494e302e003566904a8c265d545fa2c9c6f0
Haani Khan <107712538+khanhaani22@users.noreply.github.com>
2026-02-05 19:47:28 +0000

    changes

---
+++ Student/StudentTest.java
@@ -0,0 +1,13 @@
+import Student.Student;
+import java.util.*;
+
+import Student;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<Student>();
+        students.add(new Student(1, "Alice", 16));
+        students.add(new Student(2, "Bob", 18));
+        students.add(new Student(3, "Charlie", 17));
+    }
+}
