e0242e31485c67eabf23d068e44e6fed35141b01
Haani Khan <107712538+khanhaani22@users.noreply.github.com>
2026-02-05 19:53:04 +0000

    Student Complete

--- Student/StudentTest.java
+++ Student/StudentTest.java
@@ -7,5 +7,34 @@ public class StudentTest {
         students.add(new Student(1, "Alice", 16));
         students.add(new Student(2, "Bob", 18));
         students.add(new Student(3, "Charlie", 17));
+        //sort by roll no
+        System.out.println("Sorting by roll number...");
+        Collections.sort(students);
+
+        for(Student st: students)
+        {
+            System.out.println(st.getRollNo() + " " + st.getName() + " " + st.getAge());
+        }
+        //sort by name
+        System.out.println("\nSorting by name...");
+        Collections.sort(students, new NameCompare());
+
+        for(Student st: students)
+        {
+            System.out.println(st.getRollNo() + " " + st.getName() + " " + st.getAge());
+        }
+
+        //sort by age
+        System.out.println("\nSorting by age...");
+        Collections.sort(students, new Comparator<Student>() {
+            public int compare(Student s1, Student s2) {
+                return s1.getAge() - s2.getAge();
+    }
+        });
+
+        for(Student st: students)
+        {
+            System.out.println(st.getRollNo() + " " + st.getName() + " " + st.getAge());
+        }
     }
 }
